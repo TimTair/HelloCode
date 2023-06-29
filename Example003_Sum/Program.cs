e1c2384d5e776909ecebec25c89ed0999a866a34
Timur Tairov <tair.tim@yandex.ru>
2023-06-29 13:07:14 -0400

    Example 003 Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
 ﻿
-int numberA = 36;
-int numberB = 5;
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
